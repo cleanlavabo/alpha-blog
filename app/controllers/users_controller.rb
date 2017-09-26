863b868dc9c4335003bc3c1354174290da89a405
lazybee27102 <lazybee27102@gmail.com>
2017-09-26 16:08:48 +0700

    [Func] Complete authentication function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :update]
+
   def new
     @user = User.new
   end
 
   def index
-    #debugger
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
@@ -19,14 +20,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Your account has been edited successfully"
+      flash[:success] = 'Your account has been edited successfully'
       redirect_to user_path(@user)
     else
       render 'edit'
@@ -34,12 +32,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find params[:id]
   end
 
 end
